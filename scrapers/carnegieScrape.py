97031ab159df9835cd60b1c17ec36ad379a3c115
Ole Kristian Pedersen <okpedersen@gmail.com>
2018-02-21 13:29:46 +0100

    Changed output directory for carnegieScrape

--- scrapers/carnegieScrape.py
+++ scrapers/carnegieScrape.py
@@ -2,30 +2,30 @@ import requests
 
 def main():
 
-	# iterate through pages
-	for page in range(1, 300):
+    # iterate through pages
+    for page in range(1, 300):
 
-		url = 'https://www.cs.cmu.edu/~spok/grimmtmp/'+str(page).zfill(3)+'.txt'
+        url = 'https://www.cs.cmu.edu/~spok/grimmtmp/'+str(page).zfill(3)+'.txt'
 
-		# request page
-		r = requests.get(url)
-		# check existence
-		if(r.status_code != 200):
-			continue
-		
-		# open file
-		filename = 'text'+str(page)+'.txt'
-		f = open('text/'+filename, 'w')
+        # request page
+        r = requests.get(url)
+        # check existence
+        if(r.status_code != 200):
+            continue
 
-		# isolate text from page
-		page_text = r.text
-		
-		f.write(page_text)
-	
-		# close file
-		f.close()
+        # open file
+        filename = '{:03d}.txt'.format(page)
+        f = open('../data/carnegie/'+filename, 'w')
 
-		print(filename)
+        # isolate text from page
+        page_text = r.text
+
+        f.write(page_text)
+
+        # close file
+        f.close()
+
+        print(filename)
 
 if __name__ == "__main__":
-	main()
+    main()
