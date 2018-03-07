e1131580e5714f3d1908ebf10a02442944e05921
Ole Kristian Pedersen <okpedersen@gmail.com>
2018-03-07 14:00:14 +0100

    Generalize cleanData script

--- data/scripts/cleanData.py
+++ data/scripts/cleanData.py
@@ -1,3 +1,5 @@
+import sys
+import os
 
 # Alt til lowercase
 # Mellomrom foran tegn
@@ -6,21 +8,34 @@
 
 SPECIAL_CHARS = ('.', ',', '"', ':', ';', '!', '?')
 
+def clean_text_from_file(filename):
+    with open(filename) as f:
+        text = f.read().replace('\n', ' ')
+        for c in SPECIAL_CHARS:
+            text = text.replace(c, " "+c+" ")
+        text = " ".join(text.split())
+    return text
+
+
 def main():
-    for i in range(1, 210):
-        with open('../scrapers/text/text' + str(i) + '.txt') as f:
-            text = f.read()
-            text = text.lower()
-            text = text.replace('\n', ' ')
-            for c in SPECIAL_CHARS:
-                text = text.replace(c, " "+c+" ")
-            text = " ".join(text.split())
-            with open('../clean_data/text/text' + str(i) + '.txt', 'w') as out:
-                out.write(text)
+    if len(sys.argv) < 3:
+        print("./script [input location] [output location]")
+    in_path = sys.argv[1]
+    out_path = sys.argv[2]
 
 
-
-
+    if os.path.isfile(in_path):
+        clean_text = clean_text_from_file(in_path)
+        with open(out_path) as f:
+            f.write(clean_text)
+    else:
+        if not os.path.isdir(out_path):
+            print("Invalid directory specified!")
+        for filename in os.listdir(in_path):
+            clean_text = clean_text_from_file(os.path.join(in_path, filename))
+            _, name = os.path.split(filename)
+            with open(os.path.join(out_path, name), 'w') as f:
+                f.write(clean_text)
 
 
 
