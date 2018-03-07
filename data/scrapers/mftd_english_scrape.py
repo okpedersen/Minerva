cede38b62b6a45f52ebc63a30d1d710b8fa41009
Ole Kristian Pedersen <okpedersen@gmail.com>
2018-03-07 09:09:46 +0100

    Update script to only scrape adventures
    *  MFTD search page is buggy - list all stories, and remove verses
    * Rescrape all data

--- data/scrapers/mftd_english_scrape.py
+++ data/scrapers/mftd_english_scrape.py
@@ -5,11 +5,7 @@ import os.path as path
 from bs4 import BeautifulSoup
 
 
-
-def main():
-    # story list page
-    url = "http://www.mftd.org/search.php?action=search&act=show&langname=English&author=&verse=&hq=&tq="
-
+def find_story_links_on_page(url):
     # request page
     r = requests.get(url)
     if (r.status_code != 200):
@@ -24,9 +20,19 @@ def main():
         if "action=story" in a['href']:
             story_links.append(a['href'])
 
+    return story_links
+
+
+def main():
+
+    url = "http://www.mftd.org/search.php?action=search&act=show&langname=English&author=&verse=&hq=&tq="
+    all_links = find_story_links_on_page(url)
+    url = "http://www.mftd.org/search.php?action=search&act=show&langname=English&author=&verse=1&hq=&tq="
+    verse_links = find_story_links_on_page(url)
+    story_links = set(all_links) - set(verse_links)
+
     # parse all stories
     for i, link in enumerate(story_links, 1):
-        if i < 230: continue
         url = "http://www.mftd.org/{}".format(link)
         r = requests.get(url)
         if (r.status_code != 200):
@@ -41,7 +47,9 @@ def main():
         if story_tag == None:
             print("Found None-tag! Ignoring..")
         else:
-            text = story_tag.get_text()
+            for br in story_tag.find_all("br"):
+                br.replace_with("\n")
+            text = "\n".join(p.get_text() for p in story_tag.find_all('p'))
             with open('../raw/mftd_english/{:04d}.txt'.format(i), 'w') as f:
                 f.write(text)
 
